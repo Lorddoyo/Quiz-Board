6e54e86df8d91892261873015d67821ffb1db91f
Lorddoyo <johnkojo7777@gmail.com>
2019-03-28 16:24:21 +0300

    i have just add my javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,7 @@
-
+/*this is the front end logic or*/
+$(document).ready(function() {
+      $(".clickable").click(function() {
+        $(".image-showing").toggle();
+        $(".image-hidden").toggle();
+      });
+    });
