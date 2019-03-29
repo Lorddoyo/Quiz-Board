4f376f302da99a423737a80646b57ce3c4fe5ebd
Lorddoyo <johnkojo7777@gmail.com>
2019-03-29 15:15:47 +0300

    add a new variable in js file

--- js/scripts.js
+++ js/scripts.js
@@ -2,20 +2,20 @@ var javscript = parseInt($('input:radio[name = javascript]:checked').val());
 var datatypes = parseInt($('input:radio[name = datatypes]:checked').val());
 var punctuation = parseInt($('input:radio[name = punctuation]:checked').val());
 var wow = parseInt($("input:radio[name=wow]:checked").val());
+var short = parseInt($("input:radio[name=short]:checked").val());
 
+var results= javscript + datatypes + punctuation + wow + short;
 
-
-var results= javscript + datatypes + punctuation + wow ;
-
-/*this is the front end logic or*/
+/*this is the front end logic */
 $(document).ready(function(){
     $("#form").submit(function(event){
        event.preventDefault();
        $("#display").text(results);
-      
+
        if(results > 75){
          $("#display").text ("your marks are" + results + "you are good at it!")
        }else{
+         $("#display").text ()
 
        }
     });
