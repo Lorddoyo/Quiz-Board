a539d3a5e24f289f592f3e918a71067457336b86
Lorddoyo <johnkojo7777@gmail.com>
2019-03-29 17:13:35 +0300

    added a selector tester in the js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,36 @@
-var javscript = parseInt($('input:radio[name = javascript]:checked').val());
-var datatypes = parseInt($('input:radio[name = datatypes]:checked').val());
-var punctuation = parseInt($('input:radio[name = punctuation]:checked').val());
-var wow = parseInt($("input:radio[name=wow]:checked").val());
-var short = parseInt($("input:radio[name=short]:checked").val());
 
-var results= javscript + datatypes + punctuation + wow + short;
-/*this is the front end logic */
+/*this is the Back end logic */
 $(document).ready(function(){
-  $("#form").submit(function(event){
+
+    $("#form").submit(function(event){
        event.preventDefault();
        $("#display").text(results);
+       var javscript = parseInt($("input:radio[name = javscript]:checked").val());
+       var datatypes = parseInt($("input:radio[name = datatypes]:checked").val());
+       var punctuation = parseInt($("input:radio[name = punctuation]:checked").val());
+       var wow = parseInt($("input:radio[name=wow]:checked").val());
+       var short = parseInt($("input:radio[name=short]:checked").val());
+
+
+       var results= javscript + datatypes + punctuation + wow + short;
+
+
 
        if(results > 80){
-         $("#display").text ("your marks are" + results + "you are good at it!")
-       }else if (results <= 60){
+         $("#display").text ("Your marks are " +  results  + " You are good at it!")
+       }else if (results >= 60){
          $("#display").text("AVERAGE");
        }else {
-         $("display").text("FAIL");
+         $("#display").text("FAIL");
        }
 
 
-
     });
-
-
-
-
-
-
-
-
-
-
+});
+// front end logic
+$(document).ready(function() {
+  $("p").click(function() {
+    $(".image-showing").toggle();
+    $(".image-hidden").toggle();
+  });
 });
