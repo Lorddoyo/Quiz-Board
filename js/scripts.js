a99fb834f0ba35bb0f35c8bb52ebf05c78b04ef3
Lorddoyo <johnkojo7777@gmail.com>
2019-03-29 15:43:34 +0300

    separating business logic from user logic

--- js/scripts.js
+++ js/scripts.js
@@ -5,19 +5,22 @@ var wow = parseInt($("input:radio[name=wow]:checked").val());
 var short = parseInt($("input:radio[name=short]:checked").val());
 
 var results= javscript + datatypes + punctuation + wow + short;
-
 /*this is the front end logic */
 $(document).ready(function(){
-    $("#form").submit(function(event){
+  $("#form").submit(function(event){
        event.preventDefault();
        $("#display").text(results);
 
-       if(results > 75){
+       if(results > 80){
          $("#display").text ("your marks are" + results + "you are good at it!")
-       }else{
-         $("#display").text ()
-
+       }else if (results <= 60){
+         $("#display").text("AVERAGE");
+       }else {
+         $("display").text("FAIL");
        }
+
+
+
     });
 
 
