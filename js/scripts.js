2327ae451275fd70ecb036e88b8bc8b8d7017633
Lorddoyo <johnkojo7777@gmail.com>
2019-03-29 15:00:07 +0300

    changed an id in index.HTML

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,18 @@
-var javascript = parseInt($('input:radio[name = "javascript"].checked').val());
-var datatypes = parseInt($('input:radio[name = "datatypes"].checked').val());
-var punctuation = parseInt($('input:radio[name = "punctuation"].checked').val());
-var wow = parseInt($('input:radio[name = "wow"].checked').val());
+var javscript = parseInt($('input:radio[name = javascript]:checked').val());
+var datatypes = parseInt($('input:radio[name = datatypes]:checked').val());
+var punctuation = parseInt($('input:radio[name = punctuation]:checked').val());
+var wow = parseInt($("input:radio[name=wow]:checked").val());
 
-var results= javascript + datatypes + punctuation + wow ;
+
+
+var results= javscript + datatypes + punctuation + wow ;
 
 /*this is the front end logic or*/
 $(document).ready(function(){
-  $("input[type = 'button']").click(function(event){
     $("#form").submit(function(event){
        event.preventDefault();
        $("#display").text(results);
+      
        if(results > 75){
          $("#display").text ("your marks are" + results + "you are good at it!")
        }else{
@@ -27,5 +29,4 @@ $(document).ready(function(){
 
 
 
-});
 });
