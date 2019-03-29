2760998f3938168ec36c71d289e8235f89dcb3e3
Lorddoyo <johnkojo7777@gmail.com>
2019-03-29 11:51:31 +0300

    updated content in my js

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,25 @@
-/*this is the front end logic or*/
-$(document).ready(fuction() {
-  $("#form").submit(function(){
-    var javascript = $("input :radio[name = javascript]:checked").val();
-    var datatypes= $("input :radio[name = datatypes]:checked").val();
-    var punctuation = $("input :radio[name = punctuation]:checked").val();
-    var wow = $("input :radio[name = wow]:checked").val();
+var javascript = parseInt($('input:radio[name = "javascript"].checked').val());
+var datatypes = parseInt($('input:radio[name = "datatypes"].checked').val());
+var punctuation = parseInt($('input:radio[name = "punctuation"].checked').val());
+var wow = parseInt($('input:radio[name = "wow"].checked').val());
 
-    var results = javascript + datatypes + punctuation + wow ;
-    $("score").text("your score is" + result)
-  })
+var results= javascript + datatypes + punctuation + wow ;
+
+/*this is the front end logic or*/
+$(document).ready(function(){
+  $("input[type = 'button']").click(function(event){
+    $("#form").submit(function(event){
+      $("#display").show(results);
+
+  });
+
+
+
+  /*$("score").text("your marks are"+ results);*/
+
+  document.getElementById("display").innerHTML=results;
+
+
+
+});
 });
